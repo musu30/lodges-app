79a75895554bddf621ecf01d92752c26c2e412c5
musammil30 <musammil@ysquaretechnology.com>
2022-06-22 22:18:24 +0530

    changesss

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <div class="mainwrapper">
+    <div id="maincontainer">
     <App />
     </div>
   
